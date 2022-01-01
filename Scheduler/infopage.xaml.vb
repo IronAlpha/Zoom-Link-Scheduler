58013231ecc44daad526f6870c482ce1fe915af5
IronAlpha <42409138+IronAlpha@users.noreply.github.com>
2022-01-01 22:30:21 +0900

    Create README.md '정보 보기' 내용

--- Scheduler/infopage.xaml.vb
+++ Scheduler/infopage.xaml.vb
@@ -1,6 +1,13 @@
 ﻿Class infopage
     Private Sub Page_Loaded(sender As Object, e As RoutedEventArgs)
         wp.Children.Clear()
-        wp.Children.Add(New TextBlock With {.Text = My.Resources.nanumlicense, .MaxWidth = wp.ActualWidth, .Height = Double.NaN, .FontFamily = New FontFamily("나눔스퀘어_ac"), .FontSize = 12, .TextWrapping = TextWrapping.Wrap})
+        Dim txt As String = "줌 / 링크 자동 접속기
+github.com/IronAlpha/Zoom-Link-Scheduler/
+이메일:ironalph125@gmail.com
+
+줌이나 링크를 시간에 맞춰서 자동으로 접속하게 해주는 프로그램입니다!
+감사합니다!
+"
+        wp.Children.Add(New TextBlock With {.Text = txt, .MaxWidth = wp.ActualWidth, .Height = Double.NaN, .FontFamily = New FontFamily("나눔스퀘어_ac"), .FontSize = 12, .TextWrapping = TextWrapping.Wrap})
     End Sub
 End Class
